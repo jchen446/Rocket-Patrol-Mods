20d88395445c389eff1a76e12c9b857980e2c10b
jchen446 <jchen446@ucsc.edu>
2022-04-11 21:21:28 -0700

    first draft

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -13,10 +13,10 @@ class Menu extends Phaser.Scene {
     create() {
         // menu text configuration
         let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
+            fontFamily: 'Stencil Std, fantasy',
+            fontSize: '48px',
+            backgroundColor: '#000',
+            color: '#FFFFFF',
             align: 'right',
             padding: {
                 top: 5,
@@ -26,11 +26,17 @@ class Menu extends Phaser.Scene {
         }
         
         // show menu text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
+        this.add.text(game.config.width/2, game.config.height/2 - 3 * borderUISize - 3 * borderPadding, 'Sniper Wars', menuConfig).setOrigin(0.5);
+        menuConfig.fontSize = '28px';
+        //  menuConfig.backgroundColor = '#F3B141';
+        menuConfig.color = '#9999FF';
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Use ←→ arrows or mouse control to move', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'and', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, '(F) to fire or click to fire', menuConfig).setOrigin(0.5);
+        menuConfig.fontSize = '36px';
+        menuConfig.backgroundColor = '#FF0000';
         menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 4 * borderUISize + 4 * borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
 
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
